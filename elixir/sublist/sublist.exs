d7c55a203b859392ddf84f4a4a78060f325169d1
Roberto Carraretto <rc.carraretto@gmail.com>
2017-07-01 18:32:52 -0300

    elixir: sublist: move public api to the top

--- elixir/sublist/sublist.exs
+++ elixir/sublist/sublist.exs
@@ -1,4 +1,19 @@
 defmodule Sublist do
+
+  def compare(a, a), do: :equal
+
+  def compare(a, b) when length(a) <= length(b) do
+    _compare(a, b, [], [])
+  end
+
+  def compare(a, b) do
+    result = _compare(b, a, [], [])
+    case result do
+      :sublist -> :superlist
+      _ -> result
+    end
+  end
+
   defp _compare([], _b, _a2, _b2), do: :sublist
   defp _compare(_a, [], _a2, _b2), do: :unequal
 
@@ -13,19 +28,5 @@ defmodule Sublist do
     restored_a = Enum.reverse(a2) ++ a
     restored_b = Enum.reverse(b2) ++ b
     _compare(restored_a, tl(restored_b), [], [])
-  end
-
-  def compare(a, a), do: :equal
-
-  def compare(a, b) when length(a) <= length(b) do
-    _compare(a, b, [], [])
-  end
-
-  def compare(a, b) do
-    result = _compare(b, a, [], [])
-    case result do
-      :sublist -> :superlist
-      _ -> result
-    end
   end
 end
