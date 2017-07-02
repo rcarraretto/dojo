1f4a9a63893cb3fa11bdd945df65297b877c83eb
Roberto Carraretto <rc.carraretto@gmail.com>
2017-07-02 14:37:02 -0300

    scale generator: cleanup

--- elixir/scale-generator/scale_generator.exs
+++ elixir/scale-generator/scale_generator.exs
@@ -2,14 +2,6 @@ defmodule ScaleGenerator do
 
   @chromatic_c ~w(C C# D D# E F F# G G# A A# B)
 
-  def step(_scale, _tonic, step) do
-    case step do
-      "m" -> "C#"
-      "M" -> "D"
-      "A" -> "D#"
-    end
-  end
-
   def chromatic_scale(tonic \\ "C") do
     tonic = tonic |> String.capitalize |> sharp_notation
     rotate_to_tonic(@chromatic_c, tonic, [])
@@ -23,6 +15,14 @@ defmodule ScaleGenerator do
     case tonic in ~w[F Bb Eb Ab Db Gb d g c f bb eb] do
       true -> flat_chromatic_scale(tonic)
       false -> chromatic_scale(tonic)
+    end
+  end
+
+  def step(_scale, _tonic, step) do
+    case step do
+      "m" -> "C#"
+      "M" -> "D"
+      "A" -> "D#"
     end
   end
 
@@ -75,59 +75,3 @@ defmodule ScaleGenerator do
     chromatic_scale(note) |> Enum.at(1)
   end
 end
-
-# Find the note for a given interval (`step`) in a `scale` after the `tonic`.
-
-# "m": one semitone
-# "M": two semitones (full tone)
-# "A": augmented second (three semitones)
-
-# Given the `tonic` "D" in the `scale` (C C# D D# E F F# G G# A A# B C), you
-# should return the following notes for the given `step`:
-
-# "m": D#
-# "M": E
-# "A": F
-
-# The chromatic scale is a musical scale with thirteen pitches, each a semitone
-# (half-tone) above or below another.
-
-# Notes with a sharp (#) are a semitone higher than the note below them, where
-# the next letter note is a full tone except in the case of B and E, which have
-# no sharps.
-
-# Generate these notes, starting with the given `tonic` and wrapping back
-# around to the note before it, ending with the tonic an octave higher than the
-# original. If the `tonic` is lowercase, capitalize it.
-
-# "C" should generate: ~w(C C# D D# E F F# G G# A A# B C)
-
-# Sharp notes can also be considered the flat (b) note of the tone above them,
-# so the notes can also be represented as:
-
-# A Bb B C Db D Eb E F Gb G Ab
-
-# Generate these notes, starting with the given `tonic` and wrapping back
-# around to the note before it, ending with the tonic an octave higher than the
-# original. If the `tonic` is lowercase, capitalize it.
-
-# "C" should generate: ~w(C Db D Eb E F Gb G Ab A Bb B C)
-
-# Certain scales will require the use of the flat version, depending on the
-# `tonic` (key) that begins them, which is C in the above examples.
-
-# For any of the following tonics, use the flat chromatic scale:
-
-# F Bb Eb Ab Db Gb d g c f bb eb
-
-# For all others, use the regular chromatic scale.
-
-# The `pattern` string will let you know how many steps to make for the next
-# note in the scale.
-
-# For example, a C Major scale will receive the pattern "MMmMMMm", which
-# indicates you will start with C, make a full step over C# to D, another over
-# D# to E, then a semitone, stepping from E to F (again, E has no sharp). You
-# can follow the rest of the pattern to get:
-
-# C D E F G A B C
--- elixir/scale-generator/scale_generator_test.exs
+++ elixir/scale-generator/scale_generator_test.exs
@@ -22,20 +22,6 @@ defmodule ScaleGeneratorTest do
   # @pentatonic_scale_pattern      "MMAMA"
   # @enigmatic_scale_pattern       "mAMMMmm"
 
-  describe "step to next note" do
-    test "with half-tone interval" do
-      assert ScaleGenerator.step(~w(C C# D D# E F F# G G# A A# B), "C", "m") == "C#"
-    end
-
-    test "with full tone interval" do
-      assert ScaleGenerator.step(~w(C C# D D# E F F# G G# A A# B), "C", "M") == "D"
-    end
-
-    test "with accidental interval" do
-      assert ScaleGenerator.step(~w(C C# D D# E F F# G G# A A# B), "C", "A") == "D#"
-    end
-  end
-
   test "chromatic scale" do
     assert ScaleGenerator.chromatic_scale("C") == ~w(C C# D D# E F F# G G# A A# B C)
     assert ScaleGenerator.chromatic_scale("A") == ~w(A A# B C C# D D# E F F# G G# A)
@@ -57,7 +43,12 @@ defmodule ScaleGeneratorTest do
   end
 
   test "find chromatic scale" do
-    # flat tonics (seems to be the complete list, based on README "use flats")
+    # Per doc block:
+    # For any of the following tonics, use the flat chromatic scale:
+    # F Bb Eb Ab Db Gb d g c f bb eb
+    # For all others, use the regular chromatic scale.
+
+    # flat tonics
     assert ScaleGenerator.find_chromatic_scale("F") == ~w(F Gb G Ab A Bb B C Db D Eb E F)
     assert ScaleGenerator.find_chromatic_scale("Bb") == ~w(Bb B C Db D Eb E F Gb G Ab A Bb)
     assert ScaleGenerator.find_chromatic_scale("Eb") == ~w(Eb E F Gb G Ab A Bb B C Db D Eb)
@@ -71,104 +62,41 @@ defmodule ScaleGeneratorTest do
     assert ScaleGenerator.find_chromatic_scale("bb") == ~w(Bb B C Db D Eb E F Gb G Ab A Bb)
     assert ScaleGenerator.find_chromatic_scale("eb") == ~w(Eb E F Gb G Ab A Bb B C Db D Eb)
 
-    # non-flat tonics (seems to be all the others)
-    # seems to be incomplete (missing minors: a, e, b, f#, c#, g#, d# minor)
-
-    # based on README "no accidentals"
+    # non-flat tonics
     assert ScaleGenerator.find_chromatic_scale("C") == ~w(C C# D D# E F F# G G# A A# B C)
-
-    # based on README "use sharps"
     assert ScaleGenerator.find_chromatic_scale("G") == ~w(G G# A A# B C C# D D# E F F# G)
     assert ScaleGenerator.find_chromatic_scale("D") == ~w(D D# E F F# G G# A A# B C C# D)
     assert ScaleGenerator.find_chromatic_scale("A") == ~w(A A# B C C# D D# E F F# G G# A)
     assert ScaleGenerator.find_chromatic_scale("E") == ~w(E F F# G G# A A# B C C# D D# E)
     assert ScaleGenerator.find_chromatic_scale("B") == ~w(B C C# D D# E F F# G G# A A# B)
     assert ScaleGenerator.find_chromatic_scale("F#") == ~w(F# G G# A A# B C C# D D# E F F#)
-
-    # these are not on the README
-    # maybe they are sharp because their flat equivalents are flat
-    # (e.g. A# <=> Bb, C# <=> Db)
     assert ScaleGenerator.find_chromatic_scale("A#") == ~w(A# B C C# D D# E F F# G G# A A#)
     assert ScaleGenerator.find_chromatic_scale("C#") == ~w(C# D D# E F F# G G# A A# B C C#)
     assert ScaleGenerator.find_chromatic_scale("D#") == ~w(D# E F F# G G# A A# B C C# D D#)
     assert ScaleGenerator.find_chromatic_scale("G#") == ~w(G# A A# B C C# D D# E F F# G G#)
   end
 
-  # describe "generate scale from tonic and pattern" do
-  #   @tag :pending
-  #   test "C Major scale" do
-  #     assert ScaleGenerator.scale("C", @major_scale_pattern) == ~w(C D E F G A B C)
-  #   end
+  test "step to next note" do
+    assert ScaleGenerator.step(~w(C C# D D# E F F# G G# A A# B), "C", "m") == "C#"
+    assert ScaleGenerator.step(~w(C C# D D# E F F# G G# A A# B), "C", "M") == "D"
+    assert ScaleGenerator.step(~w(C C# D D# E F F# G G# A A# B), "C", "A") == "D#"
+  end
 
-  #   @tag :pending
-  #   test "G Major scale" do
-  #     assert ScaleGenerator.scale("G", @major_scale_pattern) == ~w(G A B C D E F# G)
-  #   end
-
-  #   @tag :pending
-  #   test "f# minor scale" do
-  #     assert ScaleGenerator.scale("f#", @minor_scale_pattern) == ~w(F# G# A B C# D E F#)
-  #   end
-
-  #   @tag :pending
-  #   test "b flat minor scale" do
-  #     assert ScaleGenerator.scale("bb", @minor_scale_pattern) == ~w(Bb C Db Eb F Gb Ab Bb)
-  #   end
-
-  #   @tag :pending
-  #   test "D Dorian scale" do
-  #     assert ScaleGenerator.scale("d", @dorian_scale_pattern) == ~w(D E F G A B C D)
-  #   end
-
-  #   @tag :pending
-  #   test "E flat Mixolydian scale" do
-  #     assert ScaleGenerator.scale("Eb", @mixolydian_scale_pattern) == ~w(Eb F G Ab Bb C Db Eb)
-  #   end
-
-  #   @tag :pending
-  #   test "a Lydian scale" do
-  #     assert ScaleGenerator.scale("a", @lydian_scale_pattern) == ~w(A B C# D# E F# G# A)
-  #   end
-
-  #   @tag :pending
-  #   test "e Phrygian scale" do
-  #     assert ScaleGenerator.scale("e", @phrygian_scale_pattern) == ~w(E F G A B C D E)
-  #   end
-
-  #   @tag :pending
-  #   test "g Locrian scale" do
-  #     assert ScaleGenerator.scale("g", @locrian_scale_pattern) == ~w(G Ab Bb C Db Eb F G)
-  #   end
-
-  #   @tag :pending
-  #   test "d Harmonic minor scale" do
-  #     assert ScaleGenerator.scale("d", @harmonic_minor_scale_pattern) == ~w(D E F G A Bb Db D)
-  #   end
-
-  #   @tag :pending
-  #   test "C Melodic minor scale" do
-  #     assert ScaleGenerator.scale("C", @melodic_minor_scale_pattern) == ~w(C D D# F G A B C)
-  #   end
-
-  #   @tag :pending
-  #   test "C Octatonic scale" do
-  #     assert ScaleGenerator.scale("C", @octatonic_scale_pattern) == ~w(C D D# F F# G# A B C)
-  #   end
-
-  #   @tag :pending
-  #   test "D flat Hexatonic scale" do
-  #     assert ScaleGenerator.scale("Db", @hexatonic_scale_pattern) == ~w(Db Eb F G A B Db)
-  #   end
-
-  #   @tag :pending
-  #   test "A Pentatonic scale" do
-  #     assert ScaleGenerator.scale("A", @pentatonic_scale_pattern) == ~w(A B C# E F# A)
-  #   end
-
-  #   @tag :pending
-  #   test "G Enigmatic scale" do
-  #     assert ScaleGenerator.scale("G", @enigmatic_scale_pattern) == ~w(G G# B C# D# F F# G)
-  #   end
-  # end
+  test "generate scale from tonic and pattern" do
+    # assert ScaleGenerator.scale("C", @major_scale_pattern) == ~w(C D E F G A B C)
+    # assert ScaleGenerator.scale("G", @major_scale_pattern) == ~w(G A B C D E F# G)
+    # assert ScaleGenerator.scale("f#", @minor_scale_pattern) == ~w(F# G# A B C# D E F#)
+    # assert ScaleGenerator.scale("bb", @minor_scale_pattern) == ~w(Bb C Db Eb F Gb Ab Bb)
+    # assert ScaleGenerator.scale("d", @dorian_scale_pattern) == ~w(D E F G A B C D)
+    # assert ScaleGenerator.scale("Eb", @mixolydian_scale_pattern) == ~w(Eb F G Ab Bb C Db Eb)
+    # assert ScaleGenerator.scale("a", @lydian_scale_pattern) == ~w(A B C# D# E F# G# A)
+    # assert ScaleGenerator.scale("e", @phrygian_scale_pattern) == ~w(E F G A B C D E)
+    # assert ScaleGenerator.scale("g", @locrian_scale_pattern) == ~w(G Ab Bb C Db Eb F G)
+    # assert ScaleGenerator.scale("d", @harmonic_minor_scale_pattern) == ~w(D E F G A Bb Db D)
+    # assert ScaleGenerator.scale("C", @melodic_minor_scale_pattern) == ~w(C D D# F G A B C)
+    # assert ScaleGenerator.scale("C", @octatonic_scale_pattern) == ~w(C D D# F F# G# A B C)
+    # assert ScaleGenerator.scale("Db", @hexatonic_scale_pattern) == ~w(Db Eb F G A B Db)
+    # assert ScaleGenerator.scale("A", @pentatonic_scale_pattern) == ~w(A B C# E F# A)
+    # assert ScaleGenerator.scale("G", @enigmatic_scale_pattern) == ~w(G G# B C# D# F F# G)
+  end
 end
-
