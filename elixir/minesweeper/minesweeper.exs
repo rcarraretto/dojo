db862abdad12217cfb25484ce01b945987442b6e
Roberto Carraretto <rc.carraretto@gmail.com>
2017-07-04 21:25:13 -0300

    minesweeper: going towards pattern matching for diagonals

--- elixir/minesweeper/minesweeper.exs
+++ elixir/minesweeper/minesweeper.exs
@@ -44,81 +44,80 @@ defmodule Minesweeper do
 
   defp scan_horizontal(matrix) do
     matrix
-    |> Enum.map(&left_and_right_row/1)
+    |> Enum.map(&scan_row_horizontal/1)
   end
 
   defp scan_vertical(matrix) do
     matrix
     |> transpose
-    |> Enum.map(&left_and_right_row/1)
+    |> Enum.map(&scan_row_horizontal/1)
     |> transpose
   end
 
   defp scan_diagonal(matrix) do
     matrix
+    |> pairs([])
+    |> Enum.map(&scan_diagonal_downwards/1)
   end
 
-  defp left_and_right_row(row) do
-    add_right(row, [])
-    |> add_left()
-  end
-
-  defp add_right([], acc) do
+  defp pairs([], acc) do
     Enum.reverse(acc)
   end
 
-  defp add_right([x], acc) do
-    add_right([], [x | acc])
+  defp pairs([single_row], acc) do
+    pair = [single_row, nil]
+    pairs([], [pair | acc])
   end
 
-  defp add_right(["*" | tail], acc) do
-    add_right(tail, ["*" | acc])
+  defp pairs([row | rows], acc) do
+    pair = [row, hd(rows)]
+    pairs(rows, [pair | acc])
   end
 
-  defp add_right([x, "*" | tail], acc) do
-    add_right(tail, ["*", x + 1 | acc])
+  def scan_diagonal_downwards([row, nil]) do
+    row
   end
 
-  defp add_right([x, y | tail], acc) do
-    add_right(tail, [y, x | acc])
+  def scan_diagonal_downwards(pair) do
+    # IO.inspect pair
+    _scan_diagonal_downwards(pair, [])
   end
 
-  defp add_left(row) do
-    row |> Enum.reverse |> add_right([]) |> Enum.reverse
+  # TODO
+  defp _scan_diagonal_downwards([row, _bottom_row], _acc) do
+    row
+  end
+
+  defp scan_row_horizontal(row) do
+    scan_right(row, [])
+    |> scan_left()
+  end
+
+  defp scan_right([], acc) do
+    Enum.reverse(acc)
+  end
+
+  defp scan_right([x], acc) do
+    scan_right([], [x | acc])
+  end
+
+  defp scan_right(["*" | tail], acc) do
+    scan_right(tail, ["*" | acc])
+  end
+
+  defp scan_right([x, "*" | tail], acc) do
+    scan_right(tail, ["*", x + 1 | acc])
+  end
+
+  defp scan_right([x, y | tail], acc) do
+    scan_right(tail, [y, x | acc])
+  end
+
+  defp scan_left(row) do
+    row |> Enum.reverse |> scan_right([]) |> Enum.reverse
   end
 
   defp transpose(matrix) do
     List.zip(matrix) |> Enum.map(&Tuple.to_list/1)
-  end
-
-  def diagonals(matrix) do
-    _diagonals(matrix, [])
-  end
-
-  defp _diagonals([], diagonals) do
-    Enum.reverse(diagonals)
-  end
-
-  defp _diagonals(rows, diagonals) do
-    { rows, diagonal } = _diagonal(rows, 0, { [], [] })
-    # IO.inspect rows
-    # IO.inspect diagonal
-    _diagonals(rows, [diagonal | diagonals])
-  end
-
-  defp _diagonal([], _col_index, { new_rows, diagonal }) do
-    diagonal = diagonal |> Enum.filter(fn(x) -> x != nil end)
-    { Enum.reverse(new_rows), Enum.reverse(diagonal) }
-  end
-
-  defp _diagonal([row | rows], col_index, { new_rows, diagonal }) do
-    elem = Enum.at(row, col_index)
-    diagonal = [elem | diagonal]
-    new_row = List.delete_at(row, col_index)
-    new_rows = case Enum.empty?(new_row) do
-      true -> new_rows
-      false -> [new_row | new_rows]
-    end
-    _diagonal(rows, col_index + 1, { new_rows, diagonal })
   end
 end
