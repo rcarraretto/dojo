07bb5c3241e02e7e875451a8d3d45bc103d1ed06
Roberto Carraretto <rc.carraretto@gmail.com>
2017-07-16 19:05:44 -0300

    poker: refactor: main method at the top

--- elixir/poker/poker.exs
+++ elixir/poker/poker.exs
@@ -12,6 +12,49 @@ defmodule Poker do
     :high_card => 9,
   }
 
+  def best_hand([hand]) do
+    [hand]
+  end
+
+  def best_hand(hands) do
+    hands
+    |> Enum.map(&with_category/1)
+    |> Enum.sort(&compare_categories/2)
+    |> filter_bests
+    |> Enum.map(&(elem(&1, 0)))
+  end
+
+  defp with_category(hand) do
+    category = categorize(hand)
+    Tuple.insert_at(category, 0, hand)
+  end
+
+  defp compare_categories({_, category, values1}, {_, category, values2}) do
+    compare_values(values1, values2)
+  end
+
+  defp compare_categories({_, category1, _}, {_, category2, _}) do
+    @category_ranks[category1] <= @category_ranks[category2]
+  end
+
+  defp compare_values([], []) do
+    true
+  end
+
+  defp compare_values([value | values1], [value | values2]) do
+    compare_values(values1, values2)
+  end
+
+  defp compare_values([value1 | _], [value2 | _]) do
+    value1 >= value2
+  end
+
+  defp filter_bests(categories) do
+    best = hd(categories)
+    categories
+    |> Enum.take_while(fn(category) -> compare_categories(category, best) end)
+  end
+
   def categorize(hand) do
     hand_t = init_hand(hand)
     groups = groups(hand_t)
@@ -22,6 +65,14 @@ defmodule Poker do
     hand
     |> hand_to_tuples
     |> Enum.sort(&compare_cards/2)
+  end
+
+  defp hand_to_tuples(hand) do
+    hand |> Enum.map(&card_to_tuple/1)
+  end
+
+  defp card_to_tuple(card) do
+    Regex.run(~r/(.*)([CDHS])/, card, capture: :all_but_first) |> List.to_tuple
   end
 
   defp groups(hand_t) do
@@ -115,14 +166,6 @@ defmodule Poker do
     group |> hd |> elem(0) |> rank_value
   end
 
-  defp hand_to_tuples(hand) do
-    hand |> Enum.map(&card_to_tuple/1)
-  end
-
-  defp card_to_tuple(card) do
-    Regex.run(~r/(.*)([CDHS])/, card, capture: :all_but_first) |> List.to_tuple
-  end
-
   defp compare_cards({rank1, _}, {rank2, _}) do
     rank_value(rank1) >= rank_value(rank2)
   end
@@ -135,48 +178,5 @@ defmodule Poker do
 
   defp to_value({rank, _}) do
     rank_value(rank)
-  end
-
-  def best_hand([hand]) do
-    [hand]
-  end
-
-  def best_hand(hands) do
-    hands
-    |> Enum.map(&with_category/1)
-    |> Enum.sort(&compare_categories/2)
-    |> filter_bests
-    |> Enum.map(&(elem(&1, 0)))
-  end
-
-  defp with_category(hand) do
-    category = categorize(hand)
-    Tuple.insert_at(category, 0, hand)
-  end
-
-  defp compare_categories({_, category, values1}, {_, category, values2}) do
-    compare_values(values1, values2)
-  end
-
-  defp compare_categories({_, category1, _}, {_, category2, _}) do
-    @category_ranks[category1] <= @category_ranks[category2]
-  end
-
-  defp compare_values([], []) do
-    true
-  end
-
-  defp compare_values([value | values1], [value | values2]) do
-    compare_values(values1, values2)
-  end
-
-  defp compare_values([value1 | _], [value2 | _]) do
-    value1 >= value2
-  end
-
-  defp filter_bests(categories) do
-    best = hd(categories)
-    categories
-    |> Enum.take_while(fn(category) -> compare_categories(category, best) end)
   end
 end
