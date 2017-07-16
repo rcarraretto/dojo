ad0f6b773f6f6b1fc5f8267112b6566428d2bd49
Roberto Carraretto <rc.carraretto@gmail.com>
2017-07-16 14:34:32 -0300

    poker: support for high card

--- elixir/poker/poker.exs
+++ elixir/poker/poker.exs
@@ -1,4 +1,32 @@
 defmodule Poker do
+
+  def categorize(hand) do
+    high = hand |> hand_to_tuples |> Enum.sort(&sort_cards/2) |> List.last |> elem(0)
+    { :high_card, high }
+  end
+
+  defp hand_to_tuples(hand) do
+    hand |> Enum.map(&card_to_tuple/1)
+  end
+
+  defp card_to_tuple(card) do
+    Regex.run(~r/(.*)([CDHS])/, card, capture: :all_but_first) |> List.to_tuple
+  end
+
+  defp sort_cards({rank1, _}, {rank2, _}) do
+    rank_value(rank1) < rank_value(rank2)
+  end
+
+  defp rank_value("J"), do: 11
+  defp rank_value("Q"), do: 12
+  defp rank_value("K"), do: 13
+  defp rank_value("A"), do: 14
+  defp rank_value(rank), do: String.to_integer(rank)
+
+  def best_hand([hand]) do
+    [hand]
+  end
+
   @doc """
   Given a list of poker hands, return a list containing the highest scoring hand.
 
@@ -29,6 +57,20 @@ defmodule Poker do
   """
   @spec best_hand(list(list(String.t()))) :: list(list(String.t()))
   def best_hand(hands) do
+    h = hands
+    |> Enum.map(&with_category/1)
+    |> Enum.sort(&compare_categories/2)
+    |> List.last
+    |> elem(0)
+    [h]
+  end
+
+  defp with_category(hand) do
+    category = categorize(hand)
+    Tuple.insert_at(category, 0, hand)
+  end
+
+  defp compare_categories({_, _, high_card}, {_, _, high_card2}) do
+    rank_value(high_card) < rank_value(high_card2)
   end
 end
-
