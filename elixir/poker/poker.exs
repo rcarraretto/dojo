3bf69a3012f6433cf34cf891911ad00be80d37bd
Roberto Carraretto <rc.carraretto@gmail.com>
2017-07-21 19:53:47 -0300

    poker: refactor

--- elixir/poker/poker.exs
+++ elixir/poker/poker.exs
@@ -35,9 +35,11 @@ defmodule HandCategory do
   def for(hand) do
     cards    = hand_to_tuples(hand)
     groups   = group_by_rank(cards)
-    category = categorize_groups(cards, groups)
-    values   = category_values(category, cards, groups)
-    {category, values}
+    category = categorize_groups(groups)
+    case category do
+      :no_groups -> for_distinct(cards)
+      _ -> for_groups(category, groups)
+    end
   end
 
   defp hand_to_tuples(hand) do
@@ -62,25 +64,42 @@ defmodule HandCategory do
     |> Enum.sort(&>=/2)
   end
 
-  defp categorize_groups(cards, groups) do
+  defp categorize_groups(groups) do
     case Enum.map(groups, &(elem(&1, 0))) do
       [4, 1]       -> :four_of_a_kind
       [3, 2]       -> :full_house
       [3, 1, 1]    -> :three_of_a_kind
       [2, 2, 1]    -> :two_pair
       [2, 1, 1, 1] -> :one_pair
-      _            -> categorize_distinct(cards)
+      _            -> :no_groups
     end
   end
 
-  defp categorize_distinct(cards) do
+  defp for_groups(category, groups) do
+    cards_by_group = Enum.map(groups, &(elem(&1, 1)))
+    values = Enum.map(cards_by_group, &(value(&1)))
+    {category, values}
+  end
+
+  defp for_distinct(cards) do
     values = values(cards)
-    case [is_sequence?(values), same_suit?(cards)] do
+    straight = is_sequence?(values)
+    flush = same_suit?(cards)
+
+    category = case [straight, flush] do
       [true, true]  -> :straight_flush
       [true, false] -> :straight
       [false, true] -> :flush
       _             -> :high_card
     end
+
+    values = if straight do
+      highest_sequence_value(values)
+    else
+      values
+    end
+
+    {category, values}
   end
 
   defp is_sequence?([14, 5, 4, 3, 2]) do
@@ -98,21 +117,7 @@ defmodule HandCategory do
     |> length() == 1
   end
 
-  defp category_values(:straight, cards, _) do
-    highest_sequence_value(cards)
-  end
-
-  defp category_values(:straight_flush, cards, _) do
-    highest_sequence_value(cards)
-  end
-
-  defp category_values(_, _, groups) do
-    cards_by_group = Enum.map(groups, &(elem(&1, 1)))
-    Enum.map(cards_by_group, &(value(&1)))
-  end
-
-  defp highest_sequence_value(cards) do
-    values = values(cards)
+  defp highest_sequence_value(values) do
     case {List.first(values), List.last(values)} do
       {14, 2} -> 5
       {highest_value, _} -> highest_value
