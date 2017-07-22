23523970edab637285221606a3f9f41a8297e4a0
Roberto Carraretto <rc.carraretto@gmail.com>
2017-07-21 21:56:41 -0300

    poker: extract CardGroups

--- elixir/poker/poker.exs
+++ elixir/poker/poker.exs
@@ -13,6 +13,39 @@ defmodule Card do
   defp rank_value(rank), do: String.to_integer(rank)
 end
 
+defmodule CardGroups do
+
+  def new(cards) do
+    groups = group_by_rank(cards)
+    {categorize(groups), values(groups)}
+  end
+
+  defp group_by_rank(cards) do
+    cards
+    |> Enum.group_by(&(&1.rank))
+    |> Enum.map(fn({_rank, cards}) -> {length(cards), cards} end)
+    |> Enum.sort(&>=/2)
+  end
+
+  defp categorize(groups) do
+    case Enum.map(groups, &(elem(&1, 0))) do
+      [4, 1]       -> :four_of_a_kind
+      [3, 2]       -> :full_house
+      [3, 1, 1]    -> :three_of_a_kind
+      [2, 2, 1]    -> :two_pair
+      [2, 1, 1, 1] -> :one_pair
+      _            -> :distinct
+    end
+  end
+
+  defp values(groups) do
+    cards_by_group = Enum.map(groups, &(elem(&1, 1)))
+    Enum.map(cards_by_group, &(group_value(&1)))
+  end
+
+  defp group_value([card | _cards]), do: card.rank
+end
+
 defmodule Hand do
   defstruct cards: [], score: 0
 
@@ -50,45 +83,15 @@ defmodule HandCategory do
   @five_high_straight [14, 5, 4, 3, 2]
 
   def for(hand) do
-    cards    = hand_to_tuples(hand)
-    groups   = group_by_rank(cards)
-    category = categorize_groups(groups)
+    cards = Enum.map(hand, &Card.new/1)
+    { category, values } = CardGroups.new(cards)
     case category do
-      :distinct -> for_distinct(cards)
-      _         -> for_groups(category, groups)
+      :distinct -> for_distinct(cards, values)
+      _         -> { category, values }
     end
   end
 
-  defp hand_to_tuples(hand) do
-    hand |> Enum.map(&Card.new/1) |> Enum.sort(&>=/2)
-  end
-
-  defp group_by_rank(cards) do
-    cards
-    |> Enum.group_by(&(&1.rank))
-    |> Enum.map(fn({_rank, cards}) -> {length(cards), cards} end)
-    |> Enum.sort(&>=/2)
-  end
-
-  defp categorize_groups(groups) do
-    case Enum.map(groups, &(elem(&1, 0))) do
-      [4, 1]       -> :four_of_a_kind
-      [3, 2]       -> :full_house
-      [3, 1, 1]    -> :three_of_a_kind
-      [2, 2, 1]    -> :two_pair
-      [2, 1, 1, 1] -> :one_pair
-      _            -> :distinct
-    end
-  end
-
-  defp for_groups(category, groups) do
-    cards_by_group = Enum.map(groups, &(elem(&1, 1)))
-    values = Enum.map(cards_by_group, &(group_value(&1)))
-    {category, values}
-  end
-
-  defp for_distinct(cards) do
-    values = values(cards)
+  defp for_distinct(cards, values) do
     straight = is_sequence?(values)
     flush = same_suit?(cards)
 
@@ -123,7 +126,4 @@ defmodule HandCategory do
 
   defp highest_sequence_value(@five_high_straight), do: 5
   defp highest_sequence_value(values),              do: List.first(values)
-
-  defp group_value([card | _cards]), do: card.rank
-  defp values(cards), do: Enum.map(cards, &(&1.rank))
 end
