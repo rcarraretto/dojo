13da8190eab8f276bd059a11033e1255460030cd
Roberto Carraretto <rc.carraretto@gmail.com>
2017-08-12 14:56:51 -0300

    say: count to 10

--- elixir/say/say.exs
+++ elixir/say/say.exs
@@ -1,8 +1,22 @@
 defmodule Say do
-  @doc """
-  Translate a positive integer into English.
-  """
-  @spec in_english(integer) :: {atom, String.t}
+
+  def english(0), do: "zero"
+  def english(1), do: "one"
+  def english(2), do: "two"
+  def english(3), do: "three"
+  def english(4), do: "four"
+  def english(5), do: "five"
+  def english(6), do: "six"
+  def english(7), do: "seven"
+  def english(8), do: "eight"
+  def english(9), do: "nine"
+  def english(10), do: "ten"
+  def english(_), do: :error
+
   def in_english(number) do
+    case english(number) do
+      :error -> {:error, "number is out of range"}
+      english -> {:ok, english}
+    end
   end
 end
--- elixir/say/say_test.exs
+++ elixir/say/say_test.exs
@@ -8,14 +8,24 @@ ExUnit.configure exclude: :pending, trace: true
 defmodule SayTest do
   use ExUnit.Case
 
-  # @tag :pending
-  test "zero" do
+  test "say" do
     assert Say.in_english(0) == {:ok, "zero"}
-  end
-
-  @tag :pending
-  test "one" do
     assert Say.in_english(1) == {:ok, "one"}
+    assert Say.in_english(2) == {:ok, "two"}
+    assert Say.in_english(3) == {:ok, "three"}
+    assert Say.in_english(4) == {:ok, "four"}
+    assert Say.in_english(5) == {:ok, "five"}
+    assert Say.in_english(6) == {:ok, "six"}
+    assert Say.in_english(7) == {:ok, "seven"}
+    assert Say.in_english(8) == {:ok, "eight"}
+    assert Say.in_english(9) == {:ok, "nine"}
+    assert Say.in_english(10) == {:ok, "ten"}
+
+    # numbers below zero are out of range
+    assert Say.in_english(-1) == {:error, "number is out of range"}
+
+    # numbers above 999,999,999,999 are out of range
+    assert Say.in_english(1_000_000_000_000) == {:error, "number is out of range"}
   end
 
   @tag :pending
@@ -71,15 +81,5 @@ defmodule SayTest do
   @tag :pending
   test "a big number" do
     assert Say.in_english(987_654_321_123) == {:ok, "nine hundred eighty-seven billion six hundred fifty-four million three hundred twenty-one thousand one hundred twenty-three"}
-  end
-
-  @tag :pending
-  test "numbers below zero are out of range" do
-    assert Say.in_english(-1) == {:error, "number is out of range"}
-  end
-
-  @tag :pending
-  test "numbers above 999,999,999,999 are out of range" do
-    assert Say.in_english(1_000_000_000_000) == {:error, "number is out of range"}
   end
 end
