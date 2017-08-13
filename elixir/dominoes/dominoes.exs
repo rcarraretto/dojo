2f3920d5b580831bf71b66482e5a884d840f6dde
Roberto Carraretto <rc.carraretto@gmail.com>
2017-08-13 15:15:28 -0300

    dominoes: refactor

--- elixir/dominoes/dominoes.exs
+++ elixir/dominoes/dominoes.exs
@@ -17,10 +17,10 @@ defmodule Dominoes do
   end
 
   defp step({first_stone, last_stone}, stock) do
-    Enum.any?(stock, fn(stone) ->
-      case {last_stone, stone} do
-        {{_, x}, {x, y}} -> step({first_stone, {x, y}}, List.delete(stock, stone))
-        {{_, x}, {y, x}} -> step({first_stone, {x, y}}, List.delete(stock, stone))
+    Enum.any?(stock, fn(next_stone) ->
+      case {last_stone, next_stone} do
+        {{_, x}, {x, y}} -> step({first_stone, {x, y}}, List.delete(stock, next_stone))
+        {{_, x}, {y, x}} -> step({first_stone, {x, y}}, List.delete(stock, next_stone))
         _                -> false
       end
     end)
