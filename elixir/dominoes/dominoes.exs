446d4c5de9a73be9fa861f753c9050974ac5ca6a
Roberto Carraretto <rc.carraretto@gmail.com>
2017-08-13 12:51:28 -0300

    dominoes: first solution

--- elixir/dominoes/dominoes.exs
+++ elixir/dominoes/dominoes.exs
@@ -1,12 +1,29 @@
 defmodule Dominoes do
 
-  @type domino :: {1..6, 1..6}
+  def chain?([]) do
+    true
+  end
 
-  @doc """
-  chain?/1 takes a list of domino stones and returns boolean indicating if it's
-  possible to make a full chain
-  """
-  @spec chain?(dominoes :: [domino] | []) :: boolean
-  def chain?(dominoes) do
+  def chain?([stone | stones]) do
+    step([stone], stones)
+  end
+
+  defp step(placed, []) do
+    result = Enum.reverse(placed)
+    {l1, _} = hd(result)
+    {_, r2} = List.last(result)
+    l1 == r2
+  end
+
+  defp step(placed, stock) do
+    {_, r1} = hd(placed)
+    Enum.any?(stock, fn(stone2) ->
+      {l2, r2} = stone2
+      case {r1 == l2, r1 == r2} do
+        {true, _} -> step([{l2, r2} | placed], List.delete(stock, stone2))
+        {_, true} -> step([{r2, l2} | placed], List.delete(stock, stone2))
+        _         -> false
+      end
+    end)
   end
 end
