90909d9878669779541f545ff769efa55a96ed8b
Roberto Carraretto <rc.carraretto@gmail.com>
2017-08-26 13:59:42 -0300

    bowling: refactor

--- elixir/bowling/bowling.exs
+++ elixir/bowling/bowling.exs
@@ -1,31 +1,36 @@
 # Elixir v1.5.1
 defmodule Frame do
-  defstruct id: 1, type: :active, rolls: [], roll_index: 0, max_rolls: 2, score: 0
+  defstruct id: 1, type: :active, rolls: [], index: 0, max_rolls: 2, score: 0
 
-  def roll(frame, {roll_index, roll}) do
+  @last_frame 10
+  @pins 10
+
+  def roll(frame = %Frame{index: i}, roll) do
+    event = {i + 1, roll}
+    updated = update_frame(frame, event)
+    {updated, event}
+  end
+
+  defp update_frame(frame, {index, roll}) do
     rolls = [roll | frame.rolls]
     type = frame_type(rolls, frame.max_rolls)
-    %{frame |
-      type: type,
-      rolls: rolls,
-      roll_index: roll_index,
-      score: frame.score + roll
-    }
+    score = frame.score + roll
+    %{frame | type: type, rolls: rolls, index: index, score: score}
   end
 
   defp frame_type(rolls, max_rolls) do
     pins = Enum.sum(rolls)
     rolls_left = max_rolls - length(rolls)
     case {pins, length(rolls), rolls_left} do
-      {pins, _, _} when pins > 10 -> :overflow
-      {10,   1, _}                -> :strike
-      {10,   2, _}                -> :spare
-      {_,    _, 0}                -> :open
-      _                           -> :active
+      {pins,  _, _} when pins > @pins -> :overflow
+      {@pins, 1, _}                   -> :strike
+      {@pins, 2, _}                   -> :spare
+      {_,     _, 0}                   -> :open
+      _                               -> :active
     end
   end
 
-  def notify(frame = %Frame{id: id, roll_index: i}, {j, roll}) when id in 1..9 do
+  def notify(frame = %Frame{id: id, index: i}, {j, roll}) when id < @last_frame do
     if j - i <= num_bonus_rolls(frame) do
       %{frame | score: frame.score + roll}
     else
@@ -37,23 +42,23 @@ defmodule Frame do
     frame
   end
 
-  def next(%Frame{id: id}) when id in 1..9 do
-    %Frame{id: id + 1}
+  def next(%Frame{id: id, index: i}) when id < @last_frame do
+    %Frame{id: id + 1, index: i}
   end
 
-  def next(frame = %Frame{id: 10}) do
+  def next(frame = %Frame{id: @last_frame}) do
     num_bonus_rolls = num_bonus_rolls(frame)
-    bonus(num_bonus_rolls)
+    bonus(frame, num_bonus_rolls)
   end
 
   def next(frame = %Frame{id: :bonus}) do
     num_bonus_rolls_left = frame.max_rolls - length(frame.rolls)
-    bonus(num_bonus_rolls_left)
+    bonus(frame, num_bonus_rolls_left)
   end
 
-  defp bonus(num_bonus_rolls) do
+  defp bonus(%Frame{index: i}, num_bonus_rolls) do
     if num_bonus_rolls > 0 do
-      %Frame{id: :bonus, max_rolls: num_bonus_rolls}
+      %Frame{id: :bonus, max_rolls: num_bonus_rolls, index: i}
     end
   end
 
@@ -67,7 +72,7 @@ defmodule Frame do
 end
 
 defmodule Bowling do
-  defstruct active: nil, frames: [], roll_index: 0
+  defstruct active: nil, frames: []
 
   def start do
     %Bowling{active: %Frame{}}
@@ -82,28 +87,25 @@ defmodule Bowling do
   end
 
   def roll(game, roll) do
-    game = %{game | roll_index: game.roll_index + 1}
-    event = {game.roll_index, roll}
-    frame = Frame.roll(game.active, event)
-    game = notify_frames(game, event)
-    update_game(game, frame)
+    {frame, event} = Frame.roll(game.active, roll)
+    frames = notify(game.frames, event)
+    update_game(game, frame, frames)
   end
 
-  defp notify_frames(game, event) do
-    frames = Enum.map(game.frames, &(Frame.notify(&1, event)))
-    %{game | frames: frames}
+  defp notify(frames, event) do
+    Enum.map(frames, &(Frame.notify(&1, event)))
   end
 
-  defp update_game(_game, %Frame{type: :overflow}) do
+  defp update_game(_game, %Frame{type: :overflow}, _frames) do
     {:error, "Pin count exceeds pins on the lane"}
   end
 
-  defp update_game(game, frame = %Frame{type: :active}) do
-    %{game | active: frame}
+  defp update_game(game, frame = %Frame{type: :active}, frames) do
+    %{game | active: frame, frames: frames}
   end
 
-  defp update_game(game, frame) do
-    %{game | active: Frame.next(frame), frames: [frame | game.frames]}
+  defp update_game(game, frame, frames) do
+    %{game | active: Frame.next(frame), frames: [frame | frames]}
   end
 
   def score(game = %Bowling{active: nil}) do
