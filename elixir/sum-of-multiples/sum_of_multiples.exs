caedbd52353c216edb2d31deb464a18b58c3d9be
Roberto Carraretto <rc.carraretto@gmail.com>
2017-09-02 13:40:48 -0300

    sum of multiples: refactor

--- elixir/sum-of-multiples/sum_of_multiples.exs
+++ elixir/sum-of-multiples/sum_of_multiples.exs
@@ -1,22 +1,11 @@
 defmodule SumOfMultiples do
 
   def to(limit, factors) do
-    factors
-    |> Enum.map(&(multiples(&1, limit)))
-    |> List.flatten()
-    |> Enum.sort()
-    |> Enum.dedup()
+    1..limit - 1
+    |> Enum.filter(fn(x) ->
+      Enum.any?(factors, fn(factor) -> rem(x, factor) == 0 end)
+    end)
+    |> Enum.uniq()
     |> Enum.sum()
-  end
-
-  defp multiples(num, limit), do: _multiples(num, limit, [0])
-
-  defp _multiples(num, limit, multiples = [current | _]) do
-    next = current + num
-    if next < limit do
-      _multiples(num, limit, [next | multiples])
-    else
-      multiples
-    end
   end
 end
