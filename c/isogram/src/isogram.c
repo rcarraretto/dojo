2c9bda33fd0b3af9c164f367aba5d1ded4124cda
Roberto Carraretto <rc.carraretto@gmail.com>
2017-09-10 18:30:51 -0300

    c: isogram: indent with 2 spaces
    https://google.github.io/styleguide/cppguide.html#Spaces_vs._Tabs

--- c/isogram/src/isogram.c
+++ c/isogram/src/isogram.c
@@ -4,19 +4,19 @@
 
 bool isIsogram(char *str)
 {
-	size_t len = strlen(str);
-	int counts[26] = { 0 };
+  size_t len = strlen(str);
+  int counts[26] = { 0 };
 
-	for (size_t i = 0; i < len; i++) {
-		if (!isalpha(str[i])) {
-			continue;
-		}
-		size_t index = tolower(str[i]) - 'a';
-		if (counts[index]) {
-			return false;
-		}
-		counts[index]++;
-	}
+  for (size_t i = 0; i < len; i++) {
+    if (!isalpha(str[i])) {
+      continue;
+    }
+    size_t index = tolower(str[i]) - 'a';
+    if (counts[index]) {
+      return false;
+    }
+    counts[index]++;
+  }
 
-	return true;
+  return true;
 }
